23dd61e5d938079fd80691747d0b661cfba9d62d
Eden Kupermintz <edenk@liveperson.com>
2018-07-15 10:04:21 +0300

    initial pass of app

--- app.js
+++ app.js
@@ -25,7 +25,7 @@ function readFiles(dirname, onFileContent, onError) {
 var readdirp = require('readdirp');
 
 var settings = {
-    root: './pages/Products/',
+    root: './pages/documents/consumer experience/connector-api/examples',
     entryType: 'files',
     depth: 3
 };
