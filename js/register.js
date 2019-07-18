6034d417de47bb36cb041f1aad50327d71dd2714
Eden Kupermintz <edenk@liveperson.com>
2019-07-18 13:07:51 +0300

    bring back opacity on disable

--- js/register.js
+++ js/register.js
@@ -12,24 +12,29 @@ $(document).ready(function () {
   disableBtn();
 });
 
+//disable the free trial button
 function disableBtn () {
   trialButton = document.getElementById('registerButton');
   trialButton.disabled = true;
+  $(trialButton).removeClass('activeButton');
 }
 
+//enable the free trial button
 function enableBtn () {
   trialButton.disabled = false;
   $(trialButton).addClass('activeButton');
 }
 
+//on click on the free trial button
 function createAccount () {
     $('#captchaContainer').on('click', '#registerButton', function (event) {
       console.log('registerClicked');
-      validatePassword();
+      validateInfo();
   })
 };
 
-function validatePassword (){
+//make sure all the info submitted to the form is valid
+function validateInfo (){
   //filling variables from the form
   firstName = $('#firstName').val();
   lastName = $('#lastName').val();
@@ -37,25 +42,32 @@ function validatePassword (){
   emailAddress = $('#emailAddress').val();
   password = $('#createPassword').val();
   confirmPassword = $("#confirmPassword").val();
+  //make sure password and confirmpassword match
   if(password != confirmPassword) {
-    console.log("Passwords Don't Match");
+    //if they don't match, show an error message on screen
     $('#passwordErrorMatch').show();
   } else {
+    //if they do match, don't show the error message and hide it in case it was displayed previously
       $('#passwordErrorMatch').hide();
     }
+    //make sure all fields are filled
   if (firstName && lastName && country && emailAddress && password && confirmPassword) {
+    //if they are filled, don't show an error message and hide it in case it was displayed previously
     $('#allFields').hide();
   } else {
+    //if they aren't filled, show the error message
     $('#allFields').show();
   }
+  //if all fields were filled and the passwords match, call the request to create an account
   if ((firstName && lastName && country && emailAddress && password && confirmPassword) && (password == confirmPassword)) {
     postRequest();
   }
 }
 
 function postRequest () {
-  console.log('I posted!');
+//defining the endpoint for account creation
   const URL = 'https://uohcduank4.execute-api.us-east-2.amazonaws.com/dev/devaccount';
+//filling in request body with variables from the form
   const user ={
     firstName: firstName,
     lastName: lastName,
@@ -63,6 +75,7 @@ function postRequest () {
     email: emailAddress,
     password: password
   }
+  //using the axios module to make the request
   axios({
     method: 'post',
     url: URL,
@@ -75,6 +88,7 @@ function postRequest () {
   .catch(err=>console.log(err))
 }
 
+//a simple fuction to hide typed passwords and show them when the relevant checkbox is filled
 function showPassword() {
   var x = document.getElementsByClassName("passwordField");
   $(x).each(function () {
