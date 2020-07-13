03b2523cf8582a114719f1e295b3f2d89534ea0d
Christopher Voncina <cvoncina@liveperson.com>
2020-07-13 14:30:06 -0700

    added logic to print out base uri for any alpha or prod account

--- web-messaging/script.js
+++ web-messaging/script.js
@@ -1,4 +1,37 @@
 
+const csdsButton = document.getElementById("lp_btn_login");
+
+function retrieveDomains(csdsUrl) {
+    $.ajax({
+        url: csdsUrl,
+        headers: {
+            'Accept': 'application/json'
+        },
+        dataType: "json"
+    })
+        .done(function (data) {
+            console.log("baseuri is ", data.baseURIs);
+            if (data.baseURIs.length > 0) {
+                //sort results alphabetically
+                data.baseURIs.sort(function (a, b) {
+                    var m1 = a.service.toLowerCase();
+                    var m2 = b.service.toLowerCase();
+                    if (m1 < m2) return -1;
+                    if (m1 > m2) return 1;
+                    return 0;
+                })
+                $.each(data.baseURIs, function () {
+                    console.log(" service is ", this.service);
+                    console.log(" base uri is ", this.baseURI);
+                    // html += `<tr><td>${this.service}</td><td>${this.baseURI}</td></tr>`;
+                });
+            }
+        })
+        .fail(function () {
+            console.log("Unable to retrieve base URIs for account, please verify your account number.");
+        })
+}
+
 
 if (site) {
     // $("#lp_account").attr("disabled", "disabled");
@@ -11,59 +44,47 @@ if (site && username) {
     lpTag.identities = [];
     lpTag.identities.push(identityFn);
     console.log("hello world i am in script");
-    const usernameResult = 'lpTest' + username;
-    function identityFn(callback) {
-        callback({
-            iss: "LivePerson",
-            acr: "loa1",
-            sub: usernameResult
-        });
-    }
-
-    lpTag.sdes.push({ "type": "ctmrinfo", "info": { customerId: usernameResult } });
-
-    window.LPJsMethodName = function (callback) {
-        callback(username);
-    };
-
-    LPGetAuthenticationToken = function (callback) {
-        callback(username);
-    }
 }
-
 $("#lp_form").submit(function (e) {
     e.preventDefault();
     const site = $("#lp_account").val();
-    const username = $("#lp_username").val();
+    csdsUrl = 'https://api.liveperson.net/api/account/' + site + '/service/baseURI?version=1.0';
+    retrieveDomains(csdsUrl);
 
-    if (window.location.href.includes(username) && username === "") {
-        window.history.replaceState(null, null, window.location.pathname);
-    }
-    if (username === "") {
-        window.location.href = updateQueryStringParameter(window.location.href, "site", site);
-
-    } else {
-        const href = updateQueryStringParameter(window.location.href, "site", site);
-        window.location.href = updateQueryStringParameter(href, "username", username);
-    }
 });
+// $("#lp_form").submit(function (e) {
+//     e.preventDefault();
+//     const site = $("#lp_account").val();
+//     const username = $("#lp_username").val();
 
-function updateQueryStringParameter(uri, key, value) {
-    const re = new RegExp("([?&])" + key + "=.*?(&|$)", "i");
-    const separator = uri.indexOf('?') !== -1 ? "&" : "?";
+//     if (window.location.href.includes(username) && username === "") {
+//         window.history.replaceState(null, null, window.location.pathname);
+//     }
+//     if (username === "") {
+//         window.location.href = updateQueryStringParameter(window.location.href, "site", site);
 
-    if (uri.match(re)) {
-        return uri.replace(re, '$1' + key + "=" + value + '$2');
-    } else {
-        return uri + separator + key + "=" + value;
-    }
-}
+//     } else {
+//         const href = updateQueryStringParameter(window.location.href, "site", site);
+//         window.location.href = updateQueryStringParameter(href, "username", username);
+//     }
+// });
+
+// function updateQueryStringParameter(uri, key, value) {
+//     const re = new RegExp("([?&])" + key + "=.*?(&|$)", "i");
+//     const separator = uri.indexOf('?') !== -1 ? "&" : "?";
+
+//     if (uri.match(re)) {
+//         return uri.replace(re, '$1' + key + "=" + value + '$2');
+//     } else {
+//         return uri + separator + key + "=" + value;
+//     }
+// }
 
 
-$('#lp_lnk_setup').click(function () {
-    var isDescriptionDisplay = $('#lp_account_setup_description').css('display') === 'block';
-    $('#lp_account_setup_description').css('display', isDescriptionDisplay ? 'none' : 'block');
-});
+// $('#lp_lnk_setup').click(function () {
+//     var isDescriptionDisplay = $('#lp_account_setup_description').css('display') === 'block';
+//     $('#lp_account_setup_description').css('display', isDescriptionDisplay ? 'none' : 'block');
+// });
 
 /*
      FILE ARCHIVED ON 19:07:52 Dec 05, 2018 AND RETRIEVED FROM THE
