a0151f293efad82183180d38d299459f71e4f9c5
Christopher Voncina <cvoncina@liveperson.com>
2020-07-17 15:42:35 -0700

    fixed the category name for add widgets md file

--- _scripts/verify-documents.sh
+++ _scripts/verify-documents.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-echo "Verifying documents match the  documentsupdated yaml file "
+echo "Verifying documents match the  documentsupdated yaml file \\n \\n"
 ruby -rjson -ryaml -e "puts YAML.load_file('./_data/documentsupdated.yaml').to_json" > ./_scripts/documentsupdated.json
 node ./_scripts/verify_documents.js > ./_scripts/docOutputError.log
 cat ./_scripts/docOutputError.log
@@ -20,7 +20,7 @@ done < "$input"
 if [[ $allowCommit == "true" ]]; then
     rm -f ./_scripts/docOutputError.log
     else
-    echo 'THERE ARE ISSUES IN BUILD PLEASE REVIEW docOutputError.log UNDER THE _scripts FOLDER'
+    echo 'THERE ARE ISSUES IN BUILD PLEASE REVIEW docOutputError.log UNDER THE _scripts FOLDER BEFORE BEING ABLE TO COMMIT'
     exit 1
 fi
 
